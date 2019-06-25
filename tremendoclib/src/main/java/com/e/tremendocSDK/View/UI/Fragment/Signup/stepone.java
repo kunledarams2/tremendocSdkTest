832e6f17aa8c2331e6b3ad47a7266f049aa5aa4f
kunledarams2 <daramolaadekunle15@gmail.com>
2019-06-25 11:50:01 +0100

    sdk new update testing

--- tremendoclib/src/main/java/com/e/tremendocSDK/View/UI/Fragment/Signup/stepone.java
+++ tremendoclib/src/main/java/com/e/tremendocSDK/View/UI/Fragment/Signup/stepone.java
@@ -16,22 +16,13 @@ import com.e.tremendocSDK.View.UI.Fragment.FragmentTitled;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link stepone.OnFragmentInteractionListener} interface
+// * {@link stepone} interface
  * to handle interaction events.
  * Use the {@link stepone#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class stepone extends FragmentTitled implements FragmentChanger {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
 
     public stepone() {
         // Required empty public constructor
@@ -55,8 +46,7 @@ public class stepone extends FragmentTitled implements FragmentChanger {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+//
         }
     }
 
@@ -68,28 +58,28 @@ public class stepone extends FragmentTitled implements FragmentChanger {
     }
 
     // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+    public void onButtonPressed() {
+//        if (mListener != null) {
+//            mListener.onFragmentInteraction(uri);
+//        }
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
+//
+//    @Override
+//    public void onDetach() {
+//        super.onDetach();
+//        mListener = null;
+//    }
 
     @Override
     public void ChangeFragment(FragmentTitled fragment) {
@@ -107,8 +97,8 @@ public class stepone extends FragmentTitled implements FragmentChanger {
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
+//    public interface OnFragmentInteractionListener {
+//        // TODO: Update argument type and name
+//        void onFragmentInteraction(Uri uri);
+//    }
 }
