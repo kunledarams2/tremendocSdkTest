f51f5a4461abde3b903a14339cb8b60433ec129f
kunledarams2 <daramolaadekunle15@gmail.com>
2019-07-07 06:48:03 +0100

    sdk  progressWork2 05/7/2019

--- tremendoclib/src/main/java/com/e/tremendocSDK/View/UI/Fragment/Finddoctor/FindADoctor.java
+++ tremendoclib/src/main/java/com/e/tremendocSDK/View/UI/Fragment/Finddoctor/FindADoctor.java
@@ -142,6 +142,7 @@ public class FindADoctor extends FragmentTitled implements FragmentChanger {
 
     private void observe(DoctorViewmodel viewmodel) {
         viewmodel.getMediatorLiveData().observe(this,doctorResult -> {
+            loader.setVisibility(View.GONE);
             if(doctorResult.isSuccessful() && doctorResult.getDatalist().isEmpty()){
                 recyclerView.setVisibility(View.GONE);
                 retrylayout.setVisibility(View.VISIBLE);
