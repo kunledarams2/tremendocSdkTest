bc1254cfacfc417fd9b573e131ed85e7d8ed548e
kunledarams2 <daramolaadekunle15@gmail.com>
2019-07-12 11:49:55 +0100

    sdk  progressWork2 11/7/2019

--- app/src/main/java/com/e/tremendocsdk/MainActivity.java
+++ app/src/main/java/com/e/tremendocsdk/MainActivity.java
@@ -1,13 +1,25 @@
 package com.e.tremendocsdk;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+//import com.e.tremendocSDK.R;
+//import android.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.e.tremendocSDK.LandingActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private  LandingActivity landingActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//
+
         setContentView(R.layout.activity_main);
+    }
+    public void getView(View view){
+//        GetApi(view );
+        landingActivity.Authn("user@gmail.com ");
     }
 }
