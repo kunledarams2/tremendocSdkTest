d8d2293996e6b5708f8f36d4ab7243e7e89faafd
kunledarams2 <Adekunle@1>
2019-07-16 10:17:44 +0100

    Progresswork 16/07/2019

--- tremendoclib/src/main/java/com/e/tremendocSDK/Api/URLS.java
+++ tremendoclib/src/main/java/com/e/tremendocSDK/Api/URLS.java
@@ -6,8 +6,9 @@ public class URLS {
 
 //    private static final String IP = BuildConfig.DEBUG ?  "188.166.14.154" : "178.128.8.26";
 //    private static final String IP = BuildConfig.DEBUG ?  "188.166.14.154" : "188.166.14.154";
-    private static final String IP= BuildConfig.DEBUG  ? "192.168.88.20" : "192.168.88.20";
+//    private static final String IP= BuildConfig.DEBUG  ? "192.168.88.20" : "192.168.88.20";
 
+    private static  final String IP = BuildConfig.DEBUG ? "localhost" : "localhost";
     public static  final String SERVER = "http://" + IP + ":9000/tremendoc/api/";
 
 
